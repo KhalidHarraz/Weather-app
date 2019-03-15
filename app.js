0a27273212469ede75ce8b23fd4dacd2ca173ad2
Khalid Harraz <khalidmohamed.km82@gmail.com>
2019-03-15 13:50:27 +0200

    Weather-app

---
+++ app.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(TheP => {
+            long = TheP.coords.longtitude;
+            lat = TheP.coords.latitude;
+
+            const proxy = "http://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/2d4c1c0301517ed772e8c2066e3f0c2a/${lat},${long}`;
+
+            fetch(api)
+                .then(Gdata => {
+                    return Gdata.json();
+                })
+                .then(data => {
+                    console.log(data);
+                });
+
+        });
+
+    }
+});
