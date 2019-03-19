dc6f330e544347c960aa3880b3b08ee4817420f6
Khalid Harraz <khalidmohamed.km82@gmail.com>
2019-03-19 16:12:31 +0200

    The correction

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ window.addEventListener("load", () => {
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(TheP => {
-            long = TheP.coords.longtitude;
+            long = TheP.coords.longitude;
             lat = TheP.coords.latitude;
 
-            const proxy = "http://cors-anywhere.herokuapp.com/";
-            const api = `${proxy}https://api.darksky.net/forecast/2d4c1c0301517ed772e8c2066e3f0c2a/${lat},${long}`;
+            const proxy = `https://cors-anywhere.herokuapp.com/`;
+            const api = `${proxy}https://api.darksky.net/forecast/2d4c1c0301517ed772e8c2066e3f0c2a/${lat},${long}/`;
 
             fetch(api)
                 .then(Gdata => {
