d09823b877f2d2a8ff9c94211b44a54a834932e1
Khalid Harraz <khalidmohamed.km82@gmail.com>
2019-03-20 03:48:49 +0200

    Finally

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 window.addEventListener("load", () => {
     let long;
     let lat;
-
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+    let temperatureSection = document.querySelector(".temperature");
+    const temperatureSpan = document.querySelector(".temperature span");
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(TheP => {
             long = TheP.coords.longitude;
@@ -15,10 +19,34 @@ window.addEventListener("load", () => {
                     return Gdata.json();
                 })
                 .then(data => {
-                    console.log(data);
-                });
+                    const { temperature, summary, icon } = data.currently;
+                    //Set DOM Elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                    //FROMULLA From celsius 
+                    let celsius = (temperature -32) * (5 / 9)
+                    //Set Icons 
+                    setIcons(icon, document.querySelector(`.icon`));
 
+                    //Change the temperature celsius / Farnhiet 
+                    temperatureSection.addEventListener("click", () => {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celsius) ;
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature ;
+                        }
+                    });
+                });
         });
+    }
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, skycons[currentIcon]);
 
     }
 });
